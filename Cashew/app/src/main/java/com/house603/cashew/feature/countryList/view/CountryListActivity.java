3bf1d423aa9458fd41439c0a08f8e1519165a8c3
Adeyeye Gbenga <adeyeye546@gmail.com>
2017-03-04 06:25:46 +0700

    Countries and Capitals updated

--- Cashew/app/src/main/java/com/house603/cashew/feature/countryList/view/CountryListActivity.java
+++ Cashew/app/src/main/java/com/house603/cashew/feature/countryList/view/CountryListActivity.java
@@ -33,7 +33,38 @@ public class CountryListActivity extends BaseActionbarActivity implements Countr
     CurrencyModel mCurrencymodel;
 
     String[] countryNames = new String[]{
-            "India", "Australia", "China", "Indonesia", "Japan", "Newzealand", "Russia", "South Korea"
+            "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla",
+            "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "The Bahamas",
+            "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda",
+            "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands",
+            "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde",
+            "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands",
+            "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Côte d'Ivoire", "Croatia", "Cuba",
+            "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+            "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Faeroe Islands",
+            "Falkland Islands", "Fiji", "Finland", "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
+            "Gabon", "The Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland",
+            "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
+            "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq",
+            "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya",
+            "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
+            "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar", "Malawi", "Malaysia",
+            "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte",
+            "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique",
+            "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
+            "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
+            "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay",
+            "Peru", "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
+            "Réunion", "Romania", "Russia", "Rwanda", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia",
+            "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "São Tomé and Príncipe", "Saudi Arabia", "Senegal",
+            "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
+            "South Africa", "South Georgia and the South Sandwich Islands", "South Korea", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen",
+            "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand",
+            "Timor-Leste", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
+            "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay",
+            "US Virgin Islands", "Uzbekistan", "Vanuatu", "The Hoy See (Vatican City State)", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
+            "Yemen", "Zambia", "Zimbabwe"
+
     };
     int[] flagId = new int[]{
             R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,
@@ -42,7 +73,37 @@ public class CountryListActivity extends BaseActionbarActivity implements Countr
 //            R.drawable.flag_japan, R.drawable.flag_newzealand, R.drawable.flag_russia, R.drawable.flag_southkorea
     };
     String[] capital = new String[]{
-            "New Delhi", "Sydney", "Beijing", "Jakarta", "Tokyo", "Wellington", "Moscow", "Seoul"
+            "Kabul", "Tirana", "Algiers", "Pago Pago", "Andorra la Vella", "Luanda", "The Valley",
+            "St John's", "Buenos Aires", "Yerevan", "Oranjestad", "Canberra", "Vienna", "Baku", "Nassau",
+            "Manama", "Dhaka", "Bridgetown", "Minsk", "Brussels", "Belmopan", "Porto Novo", "Hamilton",
+            "Thimphu", "Sucre", "Sarajevo", "Gaborone", "Brasilia", "Road Town",
+            "Bandar Seri Begawan", "Sofia", "Ouagadougou", "Bujumbura", "Phnom Penh", "Yaoundé", "Ottawa", "Praia",
+            "George Town", "Bangui", "N'Djamena", "Santiago", "Beijing", "Flying Fish Cove", "Bantam",
+            "Santa Fe de Bogotá", "Moroni", "Brazzaville", "Avarua", "San José", "Yamoussoukro", "Zagreb", "Havana",
+            "Nicosia", "Prague", "Kinshasa", "Copenhagen", "Djibouti", "Roseau", "Santo Domingo",
+            "Quito", "Cairo", "San Salvador", "Malabo", "Asmara", "Tallinn", "Addis Ababa", "Thorshavn",
+            "Stanley", "Suva", "Helsinki", "Skopje", "Paris", "Cayenne", "Papeete",
+            "Libreville", "Banjul", "Tbilisi", "Berlin", "Accra", "Gibraltar", "Athens", "Nuuk",
+            "St George's", "Basse Terre", "Hagåtña", "Guatemala City", "Conakry", "Bissau", "Georgetown", "Port-au-Prince",
+            "Tegucigalpa", "Hong Kong", "Budapest", "Reykjavik", "New Delhi", "Jakarta", "Tehran", "Baghdad",
+            "Dublin", "Israel", "Rome", "Kingston", "Tokyo", "Amman", "Astana", "Nairobi",
+            "Tarawa", "Kuwait City", "Bishkek", "Vientiane", "Riga", "Beirut", "Lesotho", "Monrovia",
+            "Tripoli", "Vaduz", "Vilnius", "Luxembourg", "Macau", "Antananarivo", "Lilongwe", "Kuala Lumpur",
+            "Malé", "Bamako", "Valletta", "Majuro", "Fort-de-France", "Nouakchott", "Port Louis", "Mamoudzou",
+            "Mexico City", "Palikir", "Chisinau", "Monaco", "Ulan Bator", "Plymouth", "Rabat", "Maputo",
+            "Yangon", "Windhoek", "Yaren", "Kathmandu", "Amsterdam", "Willemstad", "Nouméa", "Wellington",
+            "Managua", "Niamey", "Abuja", "Alofi", "Kingston", "Pyongyang", "Garapan",
+            "Oslo", "Muscat", "Islamabad", "Koror", "Panama City", "Port Moresby", "Asunción",
+            "Lima", "Manila", "Adamstown", "Warsaw", "Lisbon", "San Juan", "Doha",
+            "Saint-Denis", "Bucharest", "Moscow", "Kigali", "Jamestown", "Basseterre", "Castries",
+            "Saint-Pierre", "Kingstown", "Apia", "San Marino", "São Tomé", "Riyadh", "Dakar",
+            "Belgrade", "Victoria", "Freetown", "Singapore", "Bratislava", "Ljubljana", "Honiara", "Mogadishu",
+            "Pretoria", "Seoul", "Madrid", "Colombo", "Khartoum", "Paramaribo", "Longyearbyen",
+            "Mbabane", "Stockholm", "Berne", "Damascus", "Taipei", "Dushanbe", "Dodoma", "Bangkok",
+            "Dili", "Lomé", "Fakaofo", "Nuku'alofa", "Port of Spain", "Tunis", "Ankara", "Ashgabat",
+            "Cockburn Town", "Fongafale", "Kampala", "Kiev", "Abu Dhabi", "London", "Washington DC", "Montevideo",
+            "Charlotte Amalie", "Tashkent", "Port Vila", "Vatican City", "Caracas", "Hanoi", "Mata-Utu", "Al aaiun",
+            "San'a", "Lusaka", "Harare"
     };
 
     String[] currency = new String[]{
